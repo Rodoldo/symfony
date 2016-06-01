b4353599800c0a475b45cc6005397551961cd9c1
Rodoldo <rodolfo.almeida@webrall.com>
2016-05-31 23:45:18 -0300

    Segundo Alteradção

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -17,5 +17,7 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
         ));
+        /* Rodolfo
+        */
     }
 }
